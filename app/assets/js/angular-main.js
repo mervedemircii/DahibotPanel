69ddbb7f48723444a3afb678abe0e655f1b59656
ismet <ismet.akyurekk@gmail.com>
2017-04-05 18:19:41 +0300

    Chat Panel Added

--- app/assets/js/angular-main.js
+++ app/assets/js/angular-main.js
@@ -1,9 +1,17 @@
 var app = angular.module('myApp', ["ngAnimate"]);
 app.controller('QAControl', function($scope, $http) {
+    $scope.chatData = [
+        {
+            "message" : "Hello. How can I help you?",
+            "sender" : "msg-bot"
+        }
+    ];
+
     var config = { headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
         }
     };
+
     var configJSON = { headers: {
         'Content-Type': 'application/json'
     }
@@ -74,5 +82,18 @@ app.controller('QAControl', function($scope, $http) {
 
     $scope.closeNewQA = function() {
         $scope.state = !$scope.state;
+    }
+
+    $scope.askQuestion = function(q) {
+        $scope.chatData.push({"message" : q, "sender" : "msg-user"});
+        $scope.qAsked = null;
+        var quest = {"question" : q};
+        $http.post("http://localhost:5000/bot/58ac69c6eb29aa3a897b97ac/question", quest, configJSON)
+            .then(function(response){
+                $scope.chatData.push({"message": response.data.data.answer, "sender": "msg-bot"});
+            })
+            .then(function(){
+                //document.getElementById("panelChat").scrollTop = document.getElementById("panelChat").scrollHeight;
+            });
     }
 });
